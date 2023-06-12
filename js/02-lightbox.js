39ad92f4984eabb8ac6984ed12cd0ab9bf835d1f
Mesmerisee <124400238+Mesmerisee@users.noreply.github.com>
2023-06-12 17:43:51 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryFunc = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+);
+galleryEl.insertAdjacentHTML("beforeend", createGalleryFunc.join(""));
+galleryEl.innerHTML = createGalleryFunc;
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
